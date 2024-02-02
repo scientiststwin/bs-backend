c3f5b445f3e1f3151334816e9c73dfd557dabc56
Mohammad Twin <scientists.twin@gmail.com>
2024-02-02 16:34:23 +0400

    feat: pagination were added to the end-point

--- books/views.py
+++ books/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
+from common.utils import PageNumberPagination
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    pagination_class = PageNumberPagination
