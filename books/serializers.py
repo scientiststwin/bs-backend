b8965f58b56477bc1b434c53762f6ed001931676
Mohammad Twin <scientists.twin@gmail.com>
2024-02-02 16:25:26 +0400

    feat: serializers and views of the books were added

---
+++ books/serializers.py
@@ -0,0 +1,8 @@
+from . import models
+from rest_framework import serializers
+
+class BookSerializer(serializers.ModelSerializer):
+ class Meta:
+    model = models.Book
+    fields = ['id', 'title', 'author', 'created_at', 'updated_at']
+    read_only_fields = ('id', 'created_at', 'updated_at')
