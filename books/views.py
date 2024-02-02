655edc704b4d5bd2ef931898e8bef986339d76f4
Mohammad Twin <scientists.twin@gmail.com>
2024-02-02 17:16:33 +0400

    feat: search query were added

--- books/views.py
+++ books/views.py
@@ -2,8 +2,23 @@ from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
 from common.utils import PageNumberPagination
+from django.db.models import Q
+
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     pagination_class = PageNumberPagination
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        query = self.request.query_params.get('query', None)
+
+        if query:
+            search_query = Q(
+                title__icontains=query) | Q(
+                author__icontains=query)
+            queryset = queryset.filter(search_query)
+
+        return queryset
