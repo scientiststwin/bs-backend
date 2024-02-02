8f284f1bf4f1fa1b364792e13565619fd2229b27
Mohammad Twin <scientists.twin@gmail.com>
2024-02-02 16:13:05 +0400

    feat: Book model was added with its migraion

--- books/models.py
+++ books/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    class Meta:
+        verbose_name = 'Book'
+        verbose_name_plural = 'Books'
+        get_latest_by = 'created_at'
+
+    title = models.TextField()
+    author = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
